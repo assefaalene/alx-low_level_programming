4b6028eae58bcf565ee62d33b42745aa94af7547
assefaalene <assefaalene@gmail.com>
2023-02-23 14:53:01 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers(0 to 9) except 2 & 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char n = 9;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
