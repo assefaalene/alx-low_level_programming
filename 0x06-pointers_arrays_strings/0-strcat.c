8ed4380117da76ac117d5ac555aac3a67a4f8a0f
assefaalene <assefaalene@gmail.com>
2023-03-02 02:48:22 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 
 	for (x = 0; dest[x] != '\0'; x++)
 		;
-		for (y = 0; src[y] != '\n'; y++)
+	for (y = 0; src[y] != '\0'; y++)
 		{
 			dest[x] = src[y];
 			x++;
