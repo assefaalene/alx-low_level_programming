3a2f11e9d6781340bc2aa29068a97dd3b0e8fe0b
assefaalene <assefaalene@gmail.com>
2023-03-04 05:30:55 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - computes and prints the sum of all
- * the multiples of 3 or 5 below 2024
+ * the multiples of 3 or 5 below 1024
  * Retun: Always success
  */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 	int m;
 	int sum;
 
-	for (m = 0; m < 2024; m++)
+	for (m = 0; m < 1024; m++)
 	{
 		if ((m % 3) == 0 || (m % 5 == 0))
 		{
