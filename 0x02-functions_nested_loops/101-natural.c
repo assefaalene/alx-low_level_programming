683ef30765cd90051c3b5d8249a42d082660b934
assefaalene <assefaalene@gmail.com>
2023-03-04 05:18:17 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - computes and prints the sum of all
  * the multiples of 3 or 5 below 2024
- * Retun: Always 0 (Success)
+ * Retun: Always success
  */
 int main(void)
 {
 	int m;
 	int sum;
 
-	for (m = 0; m < 1024; m++)
-		if (m % 3 == 0 || m % 5 == 0)
+	while (m = 0 && m < 2024)
+	{
+		if ((m % 3) == 0 || (m % 5 == 0))
+		{
 			sum += m;
+		}
+	m++;
+	}
 	printf("%d\n", sum);
 	return (0);
 }
