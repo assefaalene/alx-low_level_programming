d63907b8b11903a7d2ecfb5f0463661246abd199
assefaalene <assefaalene@gmail.com>
2023-03-06 05:21:19 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be printed
+ * Return: 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
