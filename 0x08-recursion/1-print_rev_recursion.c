232b77268c022bf58ad9a164ca26e5953ee160ea
assefaalene <assefaalene@gmail.com>
2023-03-08 11:36:34 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  prints a string in reverse
+ * @s: input string to be reversed
+ * Return: returns reverse a string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
