3f74b41c0b752af0e30b99a53d177ec535964af9
assefaalene <assefaalene@gmail.com>
2023-03-09 04:53:31 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -8,16 +9,24 @@
 
 int _sqrt_recursion(int n)
 {
-	int i;
+	return (_sqrt(n, 1));
+}
 
-	sqrt = i * i;
-	if (sqrt > n)
-	{
+/**
+ * _sqrt - clculates the square root of a natural number
+ * @n: input number
+ * @i: a root number of n
+ * Return: if a root is not natural number, returns to -1
+ */
+
+int _sqrt(int n, int i)
+{
+	int m;
+
+	m = i * i;
+	if (m > n)
 		return (-1);
-	}
-	if (sqrt == n)
-	{
+	if (m == n)
 		return (i);
-	}
-	return (_sqrt_recursion(n));
+	return (_sqrt(n, i + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,7 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int check_prime(int n, int i);
+int _sqrt(int n, int i);
 
 
 #endif
